62e75c3c72d7fe5f4cc582e0bf516bcedf1b3f70
Sam Youatt <sdyouatt@btinternet.com>
2021-10-16 18:14:15 +0100

    ✨ added in extra spheres and orbit camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use bevy::{
+    input::mouse::{MouseMotion, MouseWheel},
     prelude::*,
-    render::wireframe::{Wireframe, WireframePlugin},
+    render::{
+        camera::PerspectiveProjection,
+        wireframe::{Wireframe, WireframePlugin},
+    },
     wgpu::{WgpuFeature, WgpuFeatures, WgpuOptions},
 };
+
+use rust_boids::pan_orbit_camera;
 
 fn main() {
     App::build()
@@ -15,6 +21,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(WireframePlugin)
         .add_startup_system(setup.system())
+        .add_system(pan_orbit_camera::pan_orbit_camera.system())
         .run();
 }
 
@@ -23,18 +30,49 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // circle
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 1.0,
-                subdivisions: 4,
-            })),
-            material: materials.add(Color::NONE.into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..Default::default()
-        })
-        .insert(Wireframe);
+    // origin circle
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 1.0,
+            subdivisions: 4,
+        })),
+        material: materials.add(Color::ANTIQUE_WHITE.into()),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+
+    // surrounding circles
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 0.2,
+            subdivisions: 4,
+        })),
+        material: materials.add(Color::AQUAMARINE.into()),
+        transform: Transform::from_xyz(5.0, 0.0, 0.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 0.2,
+            subdivisions: 4,
+        })),
+        material: materials.add(Color::FUCHSIA.into()),
+        transform: Transform::from_xyz(0.0, 5.0, 0.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 0.2,
+            subdivisions: 4,
+        })),
+        material: materials.add(Color::YELLOW.into()),
+        transform: Transform::from_xyz(0.0, 0.0, 5.0),
+        ..Default::default()
+    });
+
+    // cones
 
     // light
     commands.spawn_bundle(LightBundle {
@@ -43,8 +81,10 @@ fn setup(
     });
 
     // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(pan_orbit_camera::PanOrbitCamera::default());
 }
